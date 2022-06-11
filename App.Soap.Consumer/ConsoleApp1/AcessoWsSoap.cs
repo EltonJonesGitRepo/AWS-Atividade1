86d94cea28a2f869bb46e807b09ade22a6776d1e
Repositorio GIT <elton.h2o@hotmail.com>
2022-06-11 08:17:51 -0300

    Implementação inicial

---
+++ App.Soap.Consumer/ConsoleApp1/AcessoWsSoap.cs
@@ -0,0 +1,30 @@
+﻿using SoapHttpClient;
+using SoapHttpClient.Enums;
+using System;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class AcessoWsSoap
+    {
+        private const string EndpointSoap = "https://www.crcind.com:443/csp/samples/SOAP.Demo.cls";
+
+        public async Task<string> ConsumoWsGenerico(string nomeAction)
+        {
+            var soapClient = new SoapClient();
+            var ns = XNamespace.Get("http://tempuri.org");
+
+            var result =
+                await soapClient.PostAsync(
+                    new Uri(EndpointSoap),
+                    SoapVersion.Soap11,
+                    body: new XElement(ns.GetName(nomeAction))
+                    , action: $"http://tempuri.org/SOAP.Demo.{nomeAction}");
+
+            var content = await result.Content.ReadAsStringAsync();
+
+            return content.ToString();
+        }
+    }
+}
---
+++ App.Soap.Consumer/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AcessoWsSoap acessoWsSoap = new AcessoWsSoap();
+          
+            Console.WriteLine("\nConsumindo a SoapAction QueryByName.\n Resultado obtido:");            
+            Console.WriteLine(await acessoWsSoap.ConsumoWsGenerico("QueryByName"));
+
+
+            Console.WriteLine("\n\n\nConsumindo a SoapAction GetListByName.\n Resultado obtido:");
+            Console.WriteLine(await acessoWsSoap.ConsumoWsGenerico("GetListByName"));
+
+
+            Console.WriteLine("\n\n\nConsumindo a SoapAction GetDataSetByName.\n Resultado obtido:");
+            Console.WriteLine(await acessoWsSoap.ConsumoWsGenerico("GetDataSetByName"));
+
+            Console.ReadKey();
+        }
+    }
+}
